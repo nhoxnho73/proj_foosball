bfe5ee2777ccdf6cabc061cb085fd4f866680253
nhoxnho73 <thanhphong7up@gmail.com>
2019-10-13 08:35:22 +0700

    fix application

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ProjFoosball
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.consider_all_requests_local = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
