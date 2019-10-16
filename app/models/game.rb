931a36ad84069c38c2352d611aa6a0f549d06b86
nhoxnho73 <thanhphong7up@gmail.com>
2019-10-16 15:39:58 +0700

    disable validate

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,6 @@ class Game < ApplicationRecord
   belongs_to :winner, :class_name => 'Team'
   belongs_to :loser, :class_name => 'Team'
   
-  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
+  # validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
   
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -5,12 +5,12 @@ class Team < ApplicationRecord
   has_many :losing_games, :class_name => 'Game', :foreign_key => 'loser_id'
   has_many :winning_matches, :class_name => 'Match', :foreign_key => 'winner_id'
 
-  validates :name, presence: true
-  validate :validate_users
+  # validates :name, presence: true
+  # validate :validate_users
 
-  def validate_users
-    errors.add(:users, 'too many users') if users.size > 2
-    errors.add(:users, 'no user') if users.size < 1
-  end
+  # def validate_users
+  #   errors.add(:users, 'too many users') if users.size > 2
+  #   errors.add(:users, 'no user') if users.size < 1
+  # end
   
 end
